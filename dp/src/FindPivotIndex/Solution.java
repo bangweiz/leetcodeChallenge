45b693d17cb5ff16365b7f15f130b48d1fbe1be3
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-07-07 20:31:56 +1000

    solved problem 724

---
+++ dp/src/FindPivotIndex/Solution.java
@@ -0,0 +1,26 @@
+package FindPivotIndex;
+
+public class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] sum = new int[n];
+        sum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+
+        int res = -1;
+        if (sum[n - 1] - sum[0] == 0) {
+            return 0;
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (sum[i - 1] == sum[n - 1] - sum[i]) {
+                return i;
+            }
+        }
+        if (sum[n - 2] == 0) {
+            return n - 1;
+        }
+        return res;
+    }
+}
